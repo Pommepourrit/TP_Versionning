01581d8025b9ae02d37b0a223a6ebdb883ad438c
Amelie Douillard <a.douillard@iia-laval.fr>
2019-03-11 15:16:23 +0100

    add partner name

--- src/application.bash
+++ src/application.bash
@@ -1,5 +1,5 @@
 ### Application.bash - 2019 -  Douillard Amelie ###
 
-echo "version 1.1"
-echo "Copyright Company - 2019"
+echo "version 1.2"
+echo "Copyright Company - 2019 - IIA"
 echo "Build : 10:00"
