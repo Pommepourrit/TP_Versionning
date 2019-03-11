40f189dfd105e5200e8ddbe36d0ef7aa0a37a2b0
Amelie Douillard <a.douillard@iia-laval.fr>
2019-03-11 15:13:37 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,5 +1,6 @@
 ### Application.bash - 2019 -  Douillard Amelie ###
+version ="2"
+build_hour="11H00"
 
-echo "version 1.1"
-echo "Copyright Company - 2019"
-echo "Build : 10:00"
+echo "Build ${build_hour}"
+echo "version ${version}"
